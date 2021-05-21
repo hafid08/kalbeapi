a0c73926cb39f8eb2c399c4f0cd46b894b48639f
hafid08 <nurhafid.firmansyah@gmail.com>
2021-05-22 06:49:44 +0700

    Add files via upload

---
+++ WebAPI/Data/KalbeDbInitializer.cs
@@ -0,0 +1,24 @@
+﻿using KalbeData;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Data
+{
+    public class KalbeDbInitializer : IKalbeDbInitializer
+    {
+        private readonly KalbeContext kalbeContext;
+
+        public KalbeDbInitializer(KalbeContext context)
+        {
+            kalbeContext = context;
+        }
+        public void InitializeAsync()
+        {
+            Console.WriteLine("Kalbe DB Initilization...");
+            // Make sure database created
+            kalbeContext.Database.CanConnect();
+        }
+    }
+}
